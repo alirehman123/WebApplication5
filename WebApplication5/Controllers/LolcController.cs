a56cf48e452c2c9b41709af073a603c56f9ab5f1
Rehman <mianrehman816@gmail.com>
2022-08-17 16:32:24 +0500

    Add Course

---
+++ WebApplication5/Controllers/LolcController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    public class LolcController : Controller
+    {
+        lolschoolEntities db = new lolschoolEntities();
+        public ActionResult Index()
+        {
+            var data=db.Records.ToList();
+            return View(data);
+        }
+        [HttpPost]
+        public ActionResult Add(Record rec)
+        {
+            db.Records.Add(rec);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
